6c0e14ce13d836cbb410b0c2b05f0202d5e17beb
chaker707 <chaker707@yahoo.fr>
2018-07-14 13:37:14 +0100

    version 1

---
+++ src/main/java/tn/enig/App6BoutiqueHerukoApplication.java
@@ -0,0 +1,12 @@
+package tn.enig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App6BoutiqueHerukoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App6BoutiqueHerukoApplication.class, args);
+	}
+}
